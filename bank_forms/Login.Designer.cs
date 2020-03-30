9f783334de28acc6a442684a148468e04110d4cd
Cola <c.7843543@gmail.com>
2020-03-30 19:21:50 +0300

    тип аутентификация

---
+++ bank_forms/Login.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace bank_forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBx_login = new System.Windows.Forms.TextBox();
+            this.txtBx_password = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_login
+            // 
+            this.Btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Btn_login.Location = new System.Drawing.Point(70, 186);
+            this.Btn_login.Name = "Btn_login";
+            this.Btn_login.Size = new System.Drawing.Size(100, 26);
+            this.Btn_login.TabIndex = 0;
+            this.Btn_login.Text = "Login";
+            this.Btn_login.UseVisualStyleBackColor = true;
+            this.Btn_login.Click += new System.EventHandler(this.Btn_login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(98, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(86, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtBx_login
+            // 
+            this.txtBx_login.Location = new System.Drawing.Point(70, 41);
+            this.txtBx_login.Name = "txtBx_login";
+            this.txtBx_login.Size = new System.Drawing.Size(100, 20);
+            this.txtBx_login.TabIndex = 3;
+            // 
+            // txtBx_password
+            // 
+            this.txtBx_password.Location = new System.Drawing.Point(70, 122);
+            this.txtBx_password.Name = "txtBx_password";
+            this.txtBx_password.Size = new System.Drawing.Size(100, 20);
+            this.txtBx_password.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 398);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 508);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtBx_password);
+            this.Controls.Add(this.txtBx_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_login);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBx_login;
+        private System.Windows.Forms.TextBox txtBx_password;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ bank_forms/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using System.Windows.Forms;
+using MongoDB.Driver;
+
+namespace bank_forms
+{
+    public partial class Login : Form
+    {
+        private MongoClient client;
+        public Login()
+        {
+            client = new MongoClient("mongodb+srv://Colba:Colba@colba-anrx1.mongodb.net/test?retryWrites=true&w=majority");
+            InitializeComponent();
+        }
+
+        private void Btn_login_Click(object sender, EventArgs e)
+        {
+            var db = client.GetDatabase("bank");
+            var collection = db.GetCollection<BsonDocument>("users");
+
+            var filter = new BsonDocument("$and", new BsonArray
+            {
+                new BsonDocument("login", txtBx_login.Text),
+                new BsonDocument("password", txtBx_password.Text)
+            }); 
+            var cursor = collection.FindSync(filter);
+            while (cursor.MoveNext())
+            {
+                var users = cursor.Current;
+                foreach (var user in users)
+                {
+                    textBox1.Text += user.ToString();
+                }
+            }
+        }
+    }
+}
