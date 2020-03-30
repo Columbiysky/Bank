ffe29345ab129bffc329789ef94a27c5711a26dc
Cola <c.7843543@gmail.com>
2020-03-30 19:44:07 +0300

    + comments

--- bank_forms/Login.cs
+++ bank_forms/Login.cs
@@ -18,26 +18,29 @@ namespace bank_forms
         public Login()
         {
             client = new MongoClient("mongodb+srv://Colba:Colba@colba-anrx1.mongodb.net/test?retryWrites=true&w=majority");
+            //Пиздец, вот и началось, коннекшин стринг, вся хуйня, сам понял
+            //Надо будет добавить твой IP в белый лист в базе монго, пока хз как это делать, я ток свой умею))
             InitializeComponent();
         }
 
         private void Btn_login_Click(object sender, EventArgs e)
         {
-            var db = client.GetDatabase("bank");
-            var collection = db.GetCollection<BsonDocument>("users");
+            var db = client.GetDatabase("bank"); //ну с ангийского переведи
+            var collection = db.GetCollection<BsonDocument>("users"); //и тут, что такое BsonDocument - до конца не понял, но мне реально поебать
 
-            var filter = new BsonDocument("$and", new BsonArray
+            var filter = new BsonDocument("$and", new BsonArray //чтобы выборку сделать ,ес что щас там только 1 юзер   
             {
-                new BsonDocument("login", txtBx_login.Text),
-                new BsonDocument("password", txtBx_password.Text)
+                new BsonDocument("login", txtBx_login.Text),            //cola
+                new BsonDocument("password", txtBx_password.Text)       //cola          защитился охуеть...
             }); 
-            var cursor = collection.FindSync(filter);
-            while (cursor.MoveNext())
+
+            var cursor = collection.FindSync(filter);       //все курсор называют, а хули мне, я не индивидуалочка тебе чтобы по своему называть
+            while (cursor.MoveNext())       
             {
-                var users = cursor.Current;
+                var users = cursor.Current;     //ну переведи ты ну что ты как ...
                 foreach (var user in users)
                 {
-                    textBox1.Text += user.ToString();
+                    textBox1.Text += user.ToString();       //ясно...
                 }
             }
         }
