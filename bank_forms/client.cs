9f8ec85cfef945a1be310f2959efe2ad70afb846
Cola <c.7843543@gmail.com>
2020-04-04 18:37:43 +0300

    класс клиента

---
+++ bank_forms/client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace bank_forms
+{
+    class Client : IClient //класс клиента, наследованный от интерфейса
+    {
+        public virtual ObjectId client_id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Login { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Surname { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual string Second_name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual int INN { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual long Phone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
