a35c0ad7b8ff235f39f3b2588526995ddacc6ab4
Cola <c.7843543@gmail.com>
2020-04-04 18:36:03 +0300

    интерфейс + класс клиента

---
+++ bank_forms/IClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace bank_forms
+{
+    interface IClient //Интерфейс клиента, тут все само собой
+    {
+        ObjectId client_id { get; set; }
+        string Login { get; set; }
+        string Password { get; set; }
+        string Surname { get; set; }
+        string Name { get; set; }
+        string Second_name { get; set; }
+        int INN { get; set; }
+        long Phone { get; set; }
+    }
+}
