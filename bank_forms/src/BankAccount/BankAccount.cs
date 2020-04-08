5060fd91d2ab5159a73de9fecb523de529398ed0
Sergey Knigin <knigin.sr@gmail.com>
2020-04-09 01:23:51 +0300

    начало класса BankAccount

---
+++ bank_forms/src/BankAccount/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace bank_forms.src.BankAccount
+{
+    class BankAccount
+    {
+        [BsonId]
+        public ObjectId IdBankAccount { get; set; }
+
+        [BsonElement("Balance")]
+        public int Balance { get; set; }
+
+        [BsonElement("StartDate")]
+        public string Startdate { get; set; }
+
+        [BsonElement("FinishDate")]
+        public string FinishDate { get; set; }
+
+        [BsonElement("IsActive")]
+        public bool IsActive { get; set; }
+    }
+}
