d21289ef95a70409ec37a984f5abac1cf5016f8b
Sergey Knigin <knigin.sr@gmail.com>
2020-04-19 22:49:24 +0300

    протестил, вроде ПРАВИЛЬНО добавляет

--- bank_forms/testAddCard.Designer.cs
+++ bank_forms/testAddCard.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.addCardBtn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // addCardBtn
             // 
-            this.addCardBtn.Location = new System.Drawing.Point(308, 91);
+            this.addCardBtn.Location = new System.Drawing.Point(97, 36);
             this.addCardBtn.Name = "addCardBtn";
             this.addCardBtn.Size = new System.Drawing.Size(158, 58);
             this.addCardBtn.TabIndex = 0;
@@ -44,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 229);
+            this.button1.Location = new System.Drawing.Point(87, 126);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 59);
             this.button1.TabIndex = 1;
@@ -52,11 +53,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 81);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create New UserBank Acc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // testAddCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1064, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.addCardBtn);
             this.Name = "testAddCard";
@@ -69,5 +81,6 @@
 
         private System.Windows.Forms.Button addCardBtn;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
